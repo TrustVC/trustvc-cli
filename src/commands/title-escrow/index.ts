b6f9ccc71537459fdd0d6ef3dca1c5162ee2d6af
Rishabh Singh <rishabh7singh5@gmail.com>
2026-01-14 14:01:23 +0530

    fix: title-escrow file moved

--- src/commands/title-escrow/index.ts
+++ src/commands/title-escrow/index.ts
@@ -5,6 +5,6 @@ export const command = 'title-escrow <method>';
 export const describe = 'Invoke a function over a title escrow smart contract on the blockchain';
 
 export const builder = (yargs: Argv): Argv =>
-  yargs.commandDir('title-escrow', { extensions: ['ts', 'js'] });
+  yargs.commandDir(__dirname, { extensions: ['ts', 'js'] });
 
 export const handler = (): void => {};
